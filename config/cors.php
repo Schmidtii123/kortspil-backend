bfdbdd0b1896ce5891fd8204769975acbff746a5
Emil Schmidt <schmidtii2000@gmail.com>
2025-11-14 14:15:15 +0100

    make sure CORS config is reading from Railway env vars

--- config/cors.php
+++ config/cors.php
@@ -3,13 +3,14 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
+    'allowed_origins' => array_filter([
+        env('FRONTEND_URL', 'https://chipper-moonbeam-100da6.netlify.app'),
         'https://chipper-moonbeam-100da6.netlify.app',
         'http://localhost:5173',
         'http://localhost:4173',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:4173',
-    ],
+    ]),
     'allowed_origins_patterns' => [
         '^http://localhost(:[0-9]+)?$',
         '^http://127\.0\.0\.1(:[0-9]+)?$',
