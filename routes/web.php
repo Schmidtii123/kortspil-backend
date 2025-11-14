18810a2b9cec2a160c0603c680be1f9e0041eeb6
Emil Schmidt <schmidtii2000@gmail.com>
2025-11-14 13:04:06 +0100

    test database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        $tables = DB::select('SHOW TABLES');
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Database connected',
+            'host' => config('database.connections.mysql.host'),
+            'database' => config('database.connections.mysql.database'),
+            'tables_count' => count($tables),
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => $e->getMessage(),
+            'host' => config('database.connections.mysql.host'),
+        ], 500);
+    }
 });
